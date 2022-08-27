bb26754315815423d23220848976542c5e5badae
Roberlande Silva <roberlanderrsilva@gmail.com>
2022-08-27 16:03:31 -0400

    auteração da senha de usuário
    foi invertida

--- criar_user.sh
+++ criar_user.sh
@@ -3,16 +3,16 @@
 
 echo "Criando usuários do sistema...."
 
-useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha321)
 passwd guest10 -e
 
-useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha321)
 passwd guest11 -e
 
-useradd guest12 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd guest12 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha321)
 passwd guest12 -e
 
-useradd guest13 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd guest13 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha321)
 passwd guest13 -e
 
 echo "Finalizado!!"
